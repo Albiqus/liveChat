573fcd71f3e688fc1a01159d9cdad6104b97777d
Альберт <albiqus@bk.ru>
2023-04-11 17:50:22 +0300

    C-7: создать окно входа в личный аккаунт, редирект
    (+) добавлен компонент входа (Login)
    (+) добавлены роуты регистрации и входа
    (+) добавлен редирект на компонент входа при успешной регистрации

--- client/src/components/LiveChat/Login/Login-styles.ts
+++ client/src/components/LiveChat/Login/Login-styles.ts
@@ -15,14 +15,10 @@ export const Div = styled.div`
     border-radius: 15px 15px 15px 15px;
 `
 
-export const Header = styled.h1`
-    font-family: 'UnivaFontBold';
-    text-align: center;
-    margin-top: 50px;
-`
-
 export const Paragraph = styled.p`
-    font-family: 'UnivaFontLight';
+    margin-top: 140px;
+    width: 100%;
+    font-family: 'UnivaFontBold';
     text-align: center;
     font-size: 25px;
 `
@@ -33,41 +29,17 @@ export const Form = styled.form`
     flex-direction: column;
     align-items: center;
 `
+
+export const Wrapper = styled.div`
+    position: relative;
+`
+
 export const Label = styled.label`
     display: block;
     text-align: center;
     margin-top: 40px;
     font-size: 20px;
     font-family: 'UnivaFontLight';
-`
-
-export const Placeholder = styled.img`
-    position: absolute;
-    right: 0;
-    top: 45px;
-    right: 30px;
-    &:hover{
-        cursor: pointer;
-    }
-`
-
-export const Tooltip = styled.div`
-    position: absolute;
-    height: auto;
-    width: 200px;
-    bottom: 55px;
-    left: 170px;
-    padding: 10px 10px 10px 10px;
-    background-color: rgb(0, 0, 0);
-    border-radius: 10px 10px 10px 0px;
-`
-
-export const P = styled.p`
-    text-align: center;
-    margin: 0 auto;
-    color: white;
-    font-family: 'custom', sans-serif;
-    font-size: 13px;
 `
 
 export const Input = styled.input`
@@ -80,9 +52,6 @@ export const Input = styled.input`
     outline: none;
 `
 
-export const Wrapper = styled.div`
-    position: relative;
-`
 
 export const ButtonBox = styled.div`
     margin-top: 70px;
---
+++ client/src/components/LiveChat/Login/SuccessRegistration/SuccessRegistration-styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SuccessRegistrationText = styled.p`
+    position: absolute;
+    margin-top: 50px;
+    width: 100%;
+    font-family: 'UnivaFontBold';
+    text-align: center;
+    font-size: 20px;
+    color: rgb( 46,139,87)
+`
